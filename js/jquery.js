e3c776e2607e78746e117b76966e0fa386131b57
Shahzaib Ishtiaq <issh5217@gmail.com>
2021-10-29 20:32:34 +0500

    New update

--- js/jquery.js
+++ js/jquery.js
@@ -1,16 +1,11 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+$(document).ready(function(){
+    $nav=$('.nav');
+    $menu=$('.nav-menu');
+    $menu1=$('.menu-a');
+    $head=$('.header');
+
+    $menu.click(function(){
+        // $nav.toggleClass('collapse');
+        // $menu1.toggleClass('collapse1');
+    })
+});
