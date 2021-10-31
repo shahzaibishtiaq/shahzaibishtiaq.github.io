e6b1999679fa6077695a35f3b980122163045326
Shahzaib Ishtiaq <issh5217@gmail.com>
2021-10-31 07:11:37 +0500

    Update 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function(){
     $(window).scroll(function(){
         if($(this).scrollTop()>100){
@@ -8,36 +9,10 @@ $(document).ready(function(){
             $('.navbar-default').removeClass('header-fixed');
         }
     });
-     $('.back-to-top').click(function(){
-        $('html,body').animate({
-            scrollTop: 0
-        },1500,'easeInOutExpo');
-         return false;
-    });
-    
-    new WOW().init();
-});
-$(document).ready(function(){
-    $('#toggle-nav-right').click(function(){
-        $('#div-response').stop().slideToggle();
-    });
-});
-$(document).ready(function(){
-  $("#owl-example").owlCarousel();
-    $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+    //  $('.back-to-top').click(function(){
+    //     $('html,body').animate({
+    //         scrollTop: 0
+    //     },1500,'easeInOutExpo');
+    //      return false;
+    // });
 });
